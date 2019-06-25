23cf537f39e2427b1de9d7ac742bb9a55801fbb6
Yurii Kovalchuk <zt1552@gmail.com>
2019-06-25 17:04:01 +0300

    3

--- app/js/common.js
+++ app/js/common.js
@@ -1,35 +1,23 @@
 ;(function($, undefined){
 
-  function addSrcImage() {
-    img = $('[data-src]');
-    var banner_arr = $.makeArray(img);
-    for (var i = 0; i < banner_arr.length; i++) {
-      console.log(banner_arr[i]);
-      var banner_item = banner_arr[i];
-      var data = banner_item.attr('data-src');
-      banner_item.attr('src', data);
-      console.log($(window).width(), banner_arr[i]);
-    }
 
-.map().map().map()
-
-
-  };
-  //
-  // function removeimage() {
-  //   img2 = $('[src]');
-  //   img2.removeAttr('src');
-  //   console.log($(window).width(), 'removeimage');
-  // };
-
-
-  $(window).width(function() {
-  if ( $(window).width() >= 850 ) {
+$(window).width(function() {
+  if ( $(window).width() >= 790 ) {
     addSrcImage();
   }
-  // if ( $(window).width() < 850 ) {
-  //   removeimage();
-  // }
 });
+
+$(window).resize(function() {
+  if ( $(window).width() >= 790 ) {
+    addSrcImage();
+  }// И теперь нужно отключить функцию слежения за шириной єкрана
+});
+
+function addSrcImage() {
+  $('[data-src]').each(function () {
+    var data = $(this).attr('data-src');
+    $(this).attr('src', data);
+  });
+};
 
 })(jQuery);
