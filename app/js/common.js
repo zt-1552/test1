131be1dc245e2c2be7bfb0afed4b9a74dc85ec23
Yurii Kovalchuk <zt1552@gmail.com>
2019-06-25 07:57:00 +0300

    2

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,35 @@
 ;(function($, undefined){
 
+  function addSrcImage() {
+    img = $('[data-src]');
+    var banner_arr = $.makeArray(img);
+    for (var i = 0; i < banner_arr.length; i++) {
+      console.log(banner_arr[i]);
+      var banner_item = banner_arr[i];
+      var data = banner_item.attr('data-src');
+      banner_item.attr('src', data);
+      console.log($(window).width(), banner_arr[i]);
+    }
+
+.map().map().map()
+
+
+  };
+  //
+  // function removeimage() {
+  //   img2 = $('[src]');
+  //   img2.removeAttr('src');
+  //   console.log($(window).width(), 'removeimage');
+  // };
+
+
+  $(window).width(function() {
+  if ( $(window).width() >= 850 ) {
+    addSrcImage();
+  }
+  // if ( $(window).width() < 850 ) {
+  //   removeimage();
+  // }
+});
+
 })(jQuery);
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,28 +1,35 @@
 ;(function($, undefined){
 
-  function getimage() {
-    img1 = $('[data-src]');
-    data = img1.attr('data-src');
-    img1.attr('src', data);
-    console.log('addimage');
+  function addSrcImage() {
+    img = $('[data-src]');
+    var banner_arr = $.makeArray(img);
+    for (var i = 0; i < banner_arr.length; i++) {
+      console.log(banner_arr[i]);
+      var banner_item = banner_arr[i];
+      var data = banner_item.attr('data-src');
+      banner_item.attr('src', data);
+      console.log($(window).width(), banner_arr[i]);
+    }
+
+.map().map().map()
+
 
   };
-
-  function removeimage() {
-    img2 = $('[src]');
-    img2.removeAttr('src');
-    console.log($(window).width(), 'removeimage');
-  };
+  //
+  // function removeimage() {
+  //   img2 = $('[src]');
+  //   img2.removeAttr('src');
+  //   console.log($(window).width(), 'removeimage');
+  // };
 
 
   $(window).width(function() {
-    console.log($(window).width());
   if ( $(window).width() >= 850 ) {
-    getimage();
+    addSrcImage();
   }
-  if ( $(window).width() < 850 ) {
-    removeimage();
-  }
+  // if ( $(window).width() < 850 ) {
+  //   removeimage();
+  // }
 });
 
 })(jQuery);
